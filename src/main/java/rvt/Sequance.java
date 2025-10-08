eae9391cade5265310b6432a2060174b45089797
A24DP3RAlfe <A240182RA@rvt.lv>
2025-10-08 15:06:07 +0300

    TheSequel

--- src/main/java/rvt/Sequance.java
+++ src/main/java/rvt/Sequance.java
@@ -16,3 +16,22 @@ public class Sequance {
     }
 }
 
+
+public class TheSequel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("First number? ");
+        int first = scanner.nextInt();
+
+        System.out.println("Last number? ");
+        int last = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = first; i <= last; i++) {
+            sum += i;
+        }
+        System.out.println("The sum is " + sum);
+        scanner.close();
+    }
+}
